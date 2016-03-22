1a42ffb50fe53fc1c72847dbaef92dfa7e0f8842
Teagan Glenn <teagan.m.glenn@gmail.com>
2016-03-22 17:30:37 -0600

    Console logger with configurable colors

---
+++ src/loggers/console.logger.js
@@ -0,0 +1,39 @@
+var colors = require('colors');
+
+var config;
+var activeLevels = {};
+var levelColors = {
+    INFO: colors.white,
+    DEBUG: colors.green,
+    WARN: colors.yellow,
+    ERROR: colors.magenta,
+    FATAL: colors.red
+};
+
+var canLog = function (logLevel) {
+    return activeLevels[logLevel];
+}
+
+var init = function (cfg) {
+    config = cfg || {};
+
+    if (config.levels) {
+        config.levels.forEach(function (logLevel) {
+            activeLevels[logLevel] = true;
+        });
+    }
+
+    if (config.colors) {
+        config.colors.forEach(function (levelColor) {
+            levelColors[levelColor.level] = colors[levelColor.color.toLowerCase()];
+        });
+    }
+};
+
+var log = function (logLevel, message) {
+    console.log(levelColors[logLevel](message));
+};
+
+exports.init = init;
+exports.log = log;
+exports.canLog = canLog;
