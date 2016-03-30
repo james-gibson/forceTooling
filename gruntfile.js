d2ee52051069cf0725f8c64df9ea361f5d7d902d
Teagan Glenn <teagan.m.glenn@gmail.com>
2016-03-30 16:50:47 -0600

    Add grunt configuration #6

--- gruntfile.js
+++ gruntfile.js
@@ -3,8 +3,8 @@
 module.exports = function(grunt) {
     grunt.config.init({
         watch:{
-            files: ['gruntfile.js', './**/*.js', 'test/**/*.js'],
-            tasks: ['eslint', 'mochaTest']
+            files: ['gruntfile.js', './**/*.js', 'test/**/*.js', './**/*.scss'],
+            tasks: ['compass:clean', 'compass:compileDevelopment', 'eslint', 'mochaTest']
         },
         eslint: {
           src: ['.']
@@ -16,10 +16,43 @@ module.exports = function(grunt) {
             },
             src: ['test/**/*.js']
           }
+        },
+        compass: {
+          clean: {
+            options: {
+              sassDir: 'resource-bundles',
+              cssDir: 'resource-bundles',
+              clean: true
+            }
+          },
+          compileDevelopment: {
+            options: {
+              sassDir: 'resource-bundles',
+              cssDir: 'resource-bundles',
+              environment: 'development'
+            }
+          },
+          watch: {
+            options: {
+              sassDir: 'resource-bundles',
+              cssDir: 'resource-bundles',
+              watch: true
+            }
+          }
+        },
+        exec: {
+          command: 'electron main.js',
+          stdout: true,
+          stderr: true
         }
       });
+
       grunt.loadNpmTasks('grunt-contrib-watch');
       grunt.loadNpmTasks('grunt-contrib-compass');
       grunt.loadNpmTasks('gruntify-eslint');
       grunt.loadNpmTasks('grunt-mocha-test');
+      grunt.loadNpmTasks('grunt-exec');
+
+      grunt.registerTask('default', ['compass:clean', 'compass:compileDevelopment', 'eslint', 'mochaTest', 'exec']);
+      grunt.registerTask('development', ['compass:clean', 'compass:compileDevelopment', 'eslint', 'mochaTest', 'exec']);
     };
